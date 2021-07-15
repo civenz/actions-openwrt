b6266a7a34716e07296a4653c40839eb97e8659f
civenz <civenz@users.noreply.github.com>
2021-07-15 08:08:08 +0800

    Update github-actions-ngrok.sh

--- github-actions-ngrok.sh
+++ github-actions-ngrok.sh
@@ -2,7 +2,7 @@
 
 LOG_FILE='/tmp/ngrok.log'
 
-curl -fsSL https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-darwin-amd64.zip -o ngrok.zip
+curl -fsSL https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip -o ngrok.zip
 unzip ngrok.zip
 chmod +x ngrok
 sudo mv ngrok /usr/local/bin
